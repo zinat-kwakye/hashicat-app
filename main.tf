c82c095588d01be8e575ba2ad859118bc65f323b
zinat-kwakye <zinatkwakye@gmail.com>
2024-03-21 11:56:36 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -198,8 +198,8 @@ resource "aws_key_pair" "hashicat" {
   public_key = tls_private_key.hashicat.public_key_openssh
 }
 
-module "s3-bucket" {
-  source              = "cloudposse/s3-bucket/aws"
+module "s3_bucket" {
+  source              = "app.terraform.io/BubblesOrganisation/s3-bucket/aws"
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
